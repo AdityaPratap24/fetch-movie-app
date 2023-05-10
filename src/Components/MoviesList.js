7e4eec671b2be0bd0d08167929c882d29a79ca6d
AdityaPratap24 <adityapratap2401@gmail.com>
2023-05-10 08:36:02 +0530

    added firebase

--- src/Components/MoviesList.js
+++ src/Components/MoviesList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Movie from './Movie';
-import classes from './MovieList.module.css';
+import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
   return (
     <ul className={classes['movies-list']}>
       {props.movies.map((movie) => (
         <Movie
+        fetchMoviesHandler={props.fetchMoviesHandler}
+          movie={movie}
+          key={movie.id}
           title={movie.title}
           releaseDate={movie.releaseDate}
           openingText={movie.openingText}
